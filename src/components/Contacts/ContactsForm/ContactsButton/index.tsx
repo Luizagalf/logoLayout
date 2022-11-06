9140bc42bcce076a7b7ae1d79e6426342abf1ef6
Luiza Galyautdinova <luizagalf@gmail.com>
2022-11-06 19:58:14 +0100

    Added animation of button

--- src/components/Contacts/ContactsForm/ContactsButton/index.tsx
+++ src/components/Contacts/ContactsForm/ContactsButton/index.tsx
@@ -1,7 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import "./contactsButton.scss";
 import plane from "assets/images/plane.svg";
-import { useState, useEffect, useRef } from "react";
+import { useRef } from "react";
 
 type ContactsButtonType = {
   action: () => void;
@@ -16,59 +15,116 @@ const ContactsButton = ({
   dirty,
   isValid
 }: ContactsButtonType) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
   const imageRef = useRef<HTMLImageElement>(null);
+  const textRef = useRef<HTMLParagraphElement>(null);
 
-  useEffect(() => {
-    getImage();
-  }, [isTouchedForm]);
+  let ticks = 0;
+  let acceleration = 1;
+  let textAcceleration = 1;
+  let animationInProgress = false;
 
-  const getImage = (action?: string) => {
-    const canvas = canvasRef.current as HTMLCanvasElement;
-    const image = imageRef.current as HTMLImageElement;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  const hover = () => {
+    const img = imageRef.current as HTMLElement;
+    const text = textRef.current as HTMLElement;
 
-    switch (action) {
-      case "hover":
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(image, 110, 10);
-        break;
-      case "click":
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(image, 200, 10);
-        break;
-      case "leave":
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(image, 50, 50);
-        break;
-      default:
-        image.onload = () => {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(image, 50, 50);
-        };
-        break;
+    ticks++;
+    acceleration += 0.02;
+    textAcceleration += 0.003;
+
+    animationInProgress = true;
+    if (ticks < 35) {
+      img.style.transform = `translate(${ticks * acceleration}px, -${
+        ticks * acceleration
+      }px)`;
+      img.style.width = `calc(${img.style.width} + 1px)`;
+      img.style.height = `calc(${img.style.height} + 1px)`;
+
+      text.style.transform = `translateY(-${ticks * textAcceleration}px)`;
+      window.requestAnimationFrame(hover);
+    } else {
+      animationInProgress = false;
+      return;
+    }
+  };
+
+  const leave = () => {
+    const img = imageRef.current as HTMLElement;
+    const text = textRef.current as HTMLElement;
+    acceleration = 1;
+    textAcceleration = 1;
+
+    animationInProgress = false;
+    if (img) {
+      img.style.transform = "";
+      img.style.width = `55.38px`;
+      img.style.height = `40px`;
+    }
+    if (text) {
+      text.style.transform = "";
+    }
+  };
+
+  const click = () => {
+    const img = imageRef.current as HTMLElement;
+    const text = textRef.current as HTMLElement;
+
+    ticks++;
+    acceleration += 0.2;
+    animationInProgress = true;
+    if (ticks < 50) {
+      img.style.transform = `translate(${ticks * acceleration}px, -${
+        ticks * acceleration
+      }px)`;
+      img.style.width = `calc(${img.style.width} -7px)`;
+      img.style.height = `calc(${img.style.height} - 7px)`;
+      text.style.transform = "";
+      window.requestAnimationFrame(click);
+    } else {
+      animationInProgress = false;
+      return;
     }
   };
 
   return (
-    <div className="contacts__btn">
+    <div className={`contacts__btn ${!isTouchedForm ? "isDone" : ""}`}>
       <div
         onMouseEnter={() => {
-          getImage("hover");
+          if (animationInProgress) {
+            return;
+          }
+          ticks = 0;
+          acceleration = 1;
+          textAcceleration = 1;
+          animationInProgress = false;
+          hover();
         }}
         onMouseLeave={() => {
-          getImage("leave");
+          ticks = 35;
+          leave();
         }}
         onClick={() => {
           if (dirty && isValid) {
-            getImage("click");
+            acceleration = 1;
+            textAcceleration = 1;
+            click();
           }
           action();
         }}
       >
-        <img src={plane} alt="" ref={imageRef} />
-        <canvas ref={canvasRef} />
-        <p>{isTouchedForm ? "Отправить" : "Ваше сообщение отправлено!"}</p>
+        {isTouchedForm ? (
+          <img
+            src={plane}
+            alt=""
+            ref={imageRef}
+            style={{ width: "55.38px", height: "40px" }}
+          />
+        ) : (
+          ""
+        )}
+
+        <p ref={textRef}>
+          {isTouchedForm ? "Отправить" : "Ваше сообщение отправлено!"}
+        </p>
       </div>
     </div>
   );
